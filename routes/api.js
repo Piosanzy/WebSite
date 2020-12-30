ee3513eb7da12960f9d90443440638840f576748
LeeInTaek <lt@happytalk.io>
2020-12-30 16:27:13 +0900

    no message

--- routes/api.js
+++ routes/api.js
@@ -13,7 +13,7 @@ router.get('/soccer/league',async function (req,res,next){
 })
 router.get('/number',async function (req,res,next){
     const number = req.query.number;
-    const a = number + 1;
+    const a = Number(number) + 1;
     res.json({number:a});
 })
 module.exports = router;
